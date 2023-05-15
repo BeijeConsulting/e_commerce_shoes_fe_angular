557186545029637c81329937fe64eddd504334b2
Paolo Di Martino <paolo.dimartino@outlook.it>
2023-05-15 15:07:21 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./screens/home/home.component";
+import { LoginFormComponent } from "./components/login-form/login-form.component";
+import { SignupFormComponent } from "./components/signup-form/signup-form.component";
+import { IdentityComponent } from "./screens/identity/identity.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ButtonComponent } from "./components/button/button.component";
+import { InputTextFieldComponent } from "./components/input-text-field/input-text-field.component";
+import { InputPasswordFieldComponent } from "./components/input-password-field/input-password-field.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginFormComponent,
+    SignupFormComponent,
+    IdentityComponent,
+    ButtonComponent,
+    InputTextFieldComponent,
+    InputPasswordFieldComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
