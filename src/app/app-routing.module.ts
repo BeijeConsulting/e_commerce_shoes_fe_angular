924f77510053784a92d33fc3b2fadeb99757f5dc
StonesCutter <martino.manzolini@beije.it>
2023-05-15 17:56:31 +0200

    Inserted the first test of a login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,80 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { DashboardComponent } from './dashboard/dashboard.component';
+// import { ContactsComponent } from './contacts/contacts.component';
+// import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+// import { ContactDetailResolver } from './resolver/contact-detail.resolver'
+import { HomeComponent } from './screens/home/home.component';
+import { IdentityComponent } from './screens/identity/identity.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: '', redirectTo: '/language', pathMatch: 'full' },
+  // {
+  //   path: ':lang', component: CmsComponent,
+  //   children: [
+       { path: '', component: HomeComponent },
+  //     {
+  //       path: 'area-personale', component: UserInfoComponent,
+  //       children: [
+  //         { path: '', component: PersonalDataComponent },
+  //         { path: 'indirizzi', component: AddressListComponent },
+  //         { path: 'ordini', component: OrderListComponent },
+  //         { path: 'lista-desideri', component: WishListComponent },
+  //       ]
+  //     },
+  //     { path: '**', component: ErrorPageComponent },
+  //     {
+  //       path: 'scarpe', component: ProductsListComponent,
+  //       children: [
+  //         { path: ':uno', component: ProductsListComponent },
+  //         { path: ':uno/:due', component: ProductsListComponent },
+  //       ]
+  //     },
+  //     { path: 'ricerca', component: SearchComponent },
+  //     { path: 'brand', component: BrandsComponent },
+  //     { path: 'brand/:brand', component: ProductsListComponent },
+  //     { path: 'scarpa/:id', component: SingleProductComponent },
+  //     { path: 'carrello', component: CartComponent },
+  //     {
+  //       path: 'assistenza', component: CustomerCareComponent,
+  //       children: [
+  //         { path: 'contatti', component: ContactsComponent },
+  //         { path: 'spedizioni', component: DeliveryComponent },
+  //         { path: 'resi', component: ReturnAndRefundComponent },
+  //         { path: 'faq', component: FaqComponent },
+  //         { path: 'cookie-policy', component: CookieComponent },
+  //         { path: 'termini-condizioni', component: TermsComponent },
+  //         { path: 'resi', component: ReturnAndRefundComponent },
+  //         { path: 'privacy-policy', component: PrivacyComponent },
+  //       ]
+  //     },
+  //   ]
+  // },
+  // { path: ':lang/checkout', component: CheckoutComponent },
+   {
+     path: ':lang/accedi', component: IdentityComponent,
+    children: [
+       { path: '', component: LoginFormComponent },
+       { path: 'registrati', component: SignupFormComponent },
+     ]
+   },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./screens/home/home.component";
@@ -11,6 +15,10 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ButtonComponent } from "./components/button/button.component";
 import { InputTextFieldComponent } from "./components/input-text-field/input-text-field.component";
 import { InputPasswordFieldComponent } from "./components/input-password-field/input-password-field.component";
+import { MatSelectModule } from '@angular/material/select'; 
+import { RouterModule, Routes } from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
@@ -22,9 +30,11 @@ import { InputPasswordFieldComponent } from "./components/input-password-field/i
     ButtonComponent,
     InputTextFieldComponent,
     InputPasswordFieldComponent,
+    // MatSelectModule
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule,   MatFormFieldModule,
+    MatInputModule, MatSelectModule, CommonModule,],
+  providers: [MatFormFieldModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
