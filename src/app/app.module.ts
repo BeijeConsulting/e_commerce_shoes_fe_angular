e30af54cd71222139b7059858601885521f20d95
StonesCutter <martino.manzolini@beije.it>
2023-05-16 11:41:23 +0200

    inserted validation functions and a bit of styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { CommonModule } from "@angular/common";
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -16,7 +17,14 @@ import { ButtonComponent } from "./components/button/button.component";
 import { InputTextFieldComponent } from "./components/input-text-field/input-text-field.component";
 import { InputPasswordFieldComponent } from "./components/input-password-field/input-password-field.component";
 import { MatSelectModule } from "@angular/material/select";
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from "@angular/router";
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -39,6 +47,15 @@ import { RouterModule, Routes } from "@angular/router";
     MatSelectModule,
     CommonModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    MatIconModule,
   ],
   providers: [MatFormFieldModule],
   bootstrap: [AppComponent],
--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
+  contactForm!: FormGroup; // Add the "!" to allow an uninitialized valueù
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      // Define your form controls with their initial values and validators
+      // For example:
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  save() {
+    // Perform form submission logic here
+  }
+
+
+  getErrorMessageEmail() {
+    if (this.email.hasError('required')) {
+      return 'You must enter an email';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+    getErrorMessagePassword() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a password';
+    }
+    return
+  }
 
 }
