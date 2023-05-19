724bf914cd3eb017e9a10c7a3aabe9666e333d32
Paolo Di Martino <paolo.dimartino@outlook.it>
2023-05-19 16:13:36 +0200

    created info-product-box component

---
+++ src/app/components/info-product-box/info-product-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-product-box',
+  templateUrl: './info-product-box.component.html',
+  styleUrls: ['./info-product-box.component.scss']
+})
+export class InfoProductBoxComponent {
+
+}
