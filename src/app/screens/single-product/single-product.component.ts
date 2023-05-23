47f05322a3a04f4f4014310285e7055feef83875
Paolo Di Martino <paolo.dimartino@outlook.it>
2023-05-23 10:56:48 +0200

    added getProduct on component mount

--- src/app/screens/single-product/single-product.component.ts
+++ src/app/screens/single-product/single-product.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductsService } from "src/app/services/products.service";
+import { TranslateService } from "@ngx-translate/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-single-product',
-  templateUrl: './single-product.component.html',
-  styleUrls: ['./single-product.component.scss']
+  selector: "app-single-product",
+  templateUrl: "./single-product.component.html",
+  styleUrls: ["./single-product.component.scss"],
 })
-export class SingleProductComponent {
+export class SingleProductComponent implements OnInit {
+  product: any;
+  productImages: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService,
+    private translateService: TranslateService
+  ) {}
+
+  ngOnInit(): void {
+    console.log("LANG", this.translateService.currentLang);
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+    console.log("ID: ", id);
+    this.productsService.getProduct(id, "it").subscribe((product) => {
+      console.log(product);
+      this.product = product;
+      this.productImages = product.images;
+    });
+  }
 }
